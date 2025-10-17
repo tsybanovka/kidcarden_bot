d7222e0757fd50a40468ab2ac86ed4a997ce368d
tsybanovka <konstantin.tsybanov@yandex.ru>
2025-10-17 17:46:24 +0300

    adding groups_teachers

--- handlers/groups_teachers/groups.py
+++ handlers/groups_teachers/groups.py
@@ -2,6 +2,8 @@ from cmath import phase
 
 from aiogram import Router, F
 from aiogram.fsm.context import FSMContext
+from aiogram.utils.media_group import MediaGroupBuilder
+
 from database import Database_read
 from filters import Main_menu
 from aiogram.types import Message, FSInputFile, InputMediaPhoto
@@ -15,40 +17,67 @@ async def baby_groups(message: Message, state: FSMContext):
 
     data = db.get_groups_teachers()["Младшая группа"]
 
-    photos = data["photos"]
-    photos = list(map(lambda x: InputMediaPhoto(media=FSInputFile("database/photos/" + x)), photos))
+    if "photos" in data.keys():
+        photos = MediaGroupBuilder(caption=data["text"])
+        for x in data["photos"]:
+            photos.add_photo(media=FSInputFile("database/photos/" + x))
 
-    await message.answer_media_group(caption=data["text"], media=photos)
+        await message.answer_media_group(caption=data["text"], media=photos.build())
+    else:
+        await message.answer(text=data["text"])
 
 @router.message(Main_menu.groups_teachers, F.text.endswith("Средние группы"))
 async def middle_groups(message: Message, state: FSMContext):
 
     data = db.get_groups_teachers()["Средняя группа"]
 
-    photos = data["photos"]
-    photos = list(map(lambda x: InputMediaPhoto(media=FSInputFile("database/photos/" + x)), photos))
+    if "photos" in data.keys():
+        photos = MediaGroupBuilder(caption=data["text"])
+        for x in data["photos"]:
+            photos.add_photo(media=FSInputFile("database/photos/" + x))
 
-    await message.answer_media_group(caption=data["text"], media=photos)
+        await message.answer_media_group(caption=data["text"], media=photos.build())
+    else:
+        await message.answer(text=data["text"])
 
 @router.message(Main_menu.groups_teachers, F.text.endswith("Старшие группы"))
 async def step_groups(message: Message, state: FSMContext):
 
     data = db.get_groups_teachers()["Старшая группа"]
 
-    photos = data["photos"]
+    if "photos" in data.keys():
+        photos = MediaGroupBuilder(caption=data["text"])
+        for x in data["photos"]:
+            photos.add_photo(media=FSInputFile("database/photos/" + x))
 
-    photos = list(map(lambda x: InputMediaPhoto(media=FSInputFile("database/photos/"+x)), photos))
-    print(photos)
-
-    await message.answer_media_group(caption=data["text"], media=photos)
+        await message.answer_media_group(caption=data["text"], media=photos.build())
+    else:
+        await message.answer(text=data["text"])
 
 @router.message(Main_menu.groups_teachers, F.text.endswith("Компенсирующие группы"))
 async def compens_groups(message: Message, state: FSMContext):
 
     data = db.get_groups_teachers()["Компенсирующая группа"]
 
-    photos = data["photos"]
-    photos = list(map(lambda x: InputMediaPhoto(media=FSInputFile("database/photos/" + x)), photos))
+    if "photos" in data.keys():
+        photos = MediaGroupBuilder(caption=data["text"])
+        for x in data["photos"]:
+            photos.add_photo(media=FSInputFile("database/photos/" + x))
 
-    await message.answer_media_group(caption=data["text"], media=photos)
+        await message.answer_media_group(caption=data["text"], media=photos.build())
+    else:
+        await message.answer(text=data["text"])
 
+@router.message(Main_menu.groups_teachers, F.text.endswith("Педагоги и специалисты"))
+async def teachers(message: Message, state: FSMContext):
+
+    data = db.get_groups_teachers()["Педагоги"]
+
+    if "photos" in data.keys():
+        photos = MediaGroupBuilder(caption=data["text"])
+        for x in data["photos"]:
+            photos.add_photo(media=FSInputFile("database/photos/" + x))
+
+        await message.answer_media_group(caption=data["text"], media=photos.build())
+    else:
+        await message.answer(text=data["text"])
